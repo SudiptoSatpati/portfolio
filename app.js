f9937a9e49d3e7fd8b7ba0bfbe45938b98b5d102
SudiptoSatpati <sudiptosatpatiofficial@gmail.com>
2024-06-29 13:45:58 +0530

    trying

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { userRouter } from "./routes/User.js";
@@ -8,6 +9,15 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+
+// Allow requests from all origins with specified options
+const corsOptions = {
+  origin: "https://667fbcd9cd6edbc48d2866b3--ssportfoliolive.netlify.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // enable passing of cookies and authorization headers
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/v1", userRouter);
 
